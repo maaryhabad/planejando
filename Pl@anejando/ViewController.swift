6fb83b0e3ca37291d0e7a7c39cb661e893dce4c3
Mariana Beilune Abad <maryh.abad@gmail.com>
2019-05-06 09:40:19 -0300

    Initial Commit

---
+++ Pl@anejando/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pl@anejando
+//
+//  Created by Mariana Beilune Abad on 06/05/19.
+//  Copyright © 2019 Mariana Beilune Abad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pl@anejandoTests/Pl_anejandoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pl_anejandoTests.swift
+//  Pl@anejandoTests
+//
+//  Created by Mariana Beilune Abad on 06/05/19.
+//  Copyright © 2019 Mariana Beilune Abad. All rights reserved.
+//
+
+import XCTest
+@testable import Pl_anejando
+
+class Pl_anejandoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
