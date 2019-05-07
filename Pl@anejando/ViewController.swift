9348568856f59313a737c9c97741175c0493cbed
Mariana Beilune Abad <maryh.abad@gmail.com>
2019-05-07 11:20:52 -0300

    troca o texto da questão antes da view carregar

--- Pl@anejando/ViewController.swift
+++ Pl@anejando/ViewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var perguntaSelecionada = 0
+    
+    var teorico = 0
+    var pragmatico = 0
+    var reflexivo = 0
+    var ativo = 0
+    
+    var questao : Questões!
 
+    @IBOutlet weak var labelQuestion: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        questao =  Model.instance.questoes[perguntaSelecionada]
+        labelQuestion.text = questao.questao
     }
+    
+
 
 
 }
