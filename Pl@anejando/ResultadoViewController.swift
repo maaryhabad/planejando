89aac57fa7eae5eb09fdc3d40a7a252f272c217d
Mariana Beilune Abad <maryh.abad@gmail.com>
2019-09-04 18:26:03 -0300

    Perguntas funcionando

--- Pl@anejando/ResultadoViewController.swift
+++ Pl@anejando/ResultadoViewController.swift
@@ -34,12 +34,12 @@ class ResultadoViewController: UIViewController {
         
     }
     
-    @IBAction func tocouCamera(_ sender: Any) {
-        let renderer = UIGraphicsImageRenderer(size: view.frame.size)
-        let image = renderer.image(actions: { context in view.layer.render(in: context.cgContext)})
-        
-        // salvar na câmera
-        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-    }
+//    @IBAction func tocouCamera(_ sender: Any) {
+//        let renderer = UIGraphicsImageRenderer(size: view.frame.size)
+//        let image = renderer.image(actions: { context in view.layer.render(in: context.cgContext)})
+//        
+//        // salvar na câmera
+//        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+//    }
     
 }
--- Pl@anejando/ViewController.swift
+++ Pl@anejando/ViewController.swift
@@ -28,55 +28,46 @@ class ViewController: UIViewController {
     
     func mostrarQuestao (){
         print("pergunta \(perguntaSelecionada)")
-        modelQuestao =  Model.instance.questoes[perguntaSelecionada]
-        questaoParaMostrar = modelQuestao.questao
-        labelQuestion.text = "\(modelQuestao.numeroDaQuestao). \(questaoParaMostrar)"
+        
+        if perguntaSelecionada < 79 {
+            modelQuestao =  Model.instance.questoes[perguntaSelecionada]
+            questaoParaMostrar = modelQuestao.questao
+            labelQuestion.text = "\(modelQuestao.numeroDaQuestao). \(questaoParaMostrar)"
+            perguntaSelecionada += 1
+        } else {
+            if let vc = self.storyboard?.instantiateViewController(withIdentifier: "fim"){
+                self.present(vc, animated: true, completion: nil)
+            }
+        }
     }
     
 
 
     @IBAction func apertouSim(_ sender: Any) {
         var estiloQuestao = Model.instance.questoes[perguntaSelecionada].estiloDaQuestao
+        
         switch estiloQuestao {
-        case 1:
-            pessoa.teórico += 1
-            print("Teórico: \(pessoa.teórico)")
-        case 2:
-            pessoa.pragmático += 1
-            print("Pragmático: \(pessoa.pragmático)")
-        case 3:
-            pessoa.reflexivo += 1
-            print("Reflexivo: \(pessoa.reflexivo)")
-        case 4:
-            pessoa.ativo += 1
-            print("Ativo: \(pessoa.ativo)")
-        default:
-            print("Não é válido")
+            case 1:
+                pessoa.teórico += 1
+                print("Teórico: \(pessoa.teórico)")
+            case 2:
+                pessoa.pragmático += 1
+                print("Pragmático: \(pessoa.pragmático)")
+            case 3:
+                pessoa.reflexivo += 1
+                print("Reflexivo: \(pessoa.reflexivo)")
+            case 4:
+                pessoa.ativo += 1
+                print("Ativo: \(pessoa.ativo)")
+            default:
+                print("Não é válido")
         }
-        
-        if perguntaSelecionada < 79 {
-            
-            
-
-            proximaQuestao()
-            
-        } else {
-            
-            if let vc = self.storyboard?.instantiateViewController(withIdentifier: "fim"){
-                self.present(vc, animated: true, completion: nil)
-            }
-        }
-        
-        
-    }
-    
-    func proximaQuestao() {
-        perguntaSelecionada += 1
         mostrarQuestao()
     }
     
+    
     @IBAction func apertouNao(_ sender: Any) {
-        proximaQuestao()
+        mostrarQuestao()
     }
     
     
